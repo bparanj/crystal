08c40207717dfbd0e5f0c050d41589235830ac88
Bala Paranj <bparanj@gmail.com>
2024-01-18 17:16:24 -0500

    Finding biggest using elixir pbt

---
+++ pbt/hello/test/big_test.exs
@@ -0,0 +1,26 @@
+defmodule BigTest do
+  use ExUnit.Case
+	use PropCheck
+
+	property "finds biggest element" do
+		forall x <- non_empty(list(integer())) do
+			biggest(x) == List.last(Enum.sort(x))
+		end
+	end
+	
+	def biggest([head | tail]) do
+		biggest(tail, head)
+	end
+	
+	defp biggest([], max) do
+		max
+	end
+	
+	defp biggest([head | tail], max) when head >= max do
+		biggest(tail, head)
+	end
+	
+	defp biggest([head | tail], max) when head < max do
+		biggest(tail, max)
+	end
+end
