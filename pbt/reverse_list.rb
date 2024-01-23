dbff1d7c0038c25c341950bcf8867527fe8dadbe
Bala Paranj <bparanj@gmail.com>
2024-01-23 13:21:04 -0500

    Specify input type and number of test cases

--- pbt/reverse_list.rb
+++ pbt/reverse_list.rb
@@ -1,6 +1,6 @@
 require_relative 'venice'
 
-# input(Array, Integer)
+input(Array, 200)
 property_reverse = -> (array) { array.reverse.reverse == array }
 
 check_reverse_property(property_reverse) 
--- pbt/venice.rb
+++ pbt/venice.rb
@@ -1,10 +1,15 @@
-def random_array
-  Array.new(rand(1..20)) { rand(-100..100) }
+def input(type, size = 100)
+  @type = type
+  @size = size
+end
+
+def random_input
+  Array.new(rand(1..20)) { rand(-100..100) if @type == Array }
 end
 
 def check_reverse_property(function)
-  100.times do
-    array = random_array
+  @size.times do
+    array = random_input
     print array
     unless function.call(array)
       puts "Test failed"
